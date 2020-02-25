fd8ac596f29fb887c870e88c09eabaf41137c526
Masaki Yoshiiwa <yopinoji@yahoo.co.jp>
2020-02-25 10:58:17 +0900

    disable userinfo in post.jsx

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -38,7 +38,7 @@ export default class PostTemplate extends React.Component {
             <PostTags tags={post.tags} />
             <SocialLinks postPath={slug} postNode={postNode} />
           </div>
-          <UserInfo config={config} />
+          {/* <UserInfo config={config} /> */}
           {/* <Disqus postNode={postNode} /> */}
         </div>
         <RelatedPosts post={post} />
