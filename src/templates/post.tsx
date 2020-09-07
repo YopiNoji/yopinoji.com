231a31de35732ae203da899395249cf5b50be090
Masaki Yoshiiwa <yopinoji@yahoo.co.jp>
2020-09-08 01:25:53 +0900

    improve style

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -11,15 +11,12 @@ const Post: React.FC<PropsType> = props => {
   const { frontmatter, html } = markdownRemark
   return (
     <Layout>
-      <div className="blog-post-container">
-        <div className="blog-post">
-          <h1>{frontmatter.title}</h1>
-          <h2>{frontmatter.date}</h2>
-          <div
-            className="blog-post-content"
-            dangerouslySetInnerHTML={{ __html: html }}
-          />
+      <div className="container">
+        <div className="mb-2">
+          <h1 className="text-xl">{frontmatter.title}</h1>
+          <h2 className="text-sm">{frontmatter.date}</h2>
         </div>
+        <div dangerouslySetInnerHTML={{ __html: html }}/>
       </div>
     </Layout>
   )
