ed340d1b855353e55357cb8aca9ec2f2d381f2a1
Masaki Yoshiiwa <yopinoji@yahoo.co.jp>
2020-09-07 22:22:22 +0900

    add markdown to post functions

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,6 +48,14 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/contents`,
+        name: `contents`,
+      },
+    },
+    `gatsby-transformer-remark`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
