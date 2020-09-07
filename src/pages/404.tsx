739619316bb5409c8381aa4b316eeaa0e2149b0a
Masaki Yoshiiwa <yopinoji@yahoo.co.jp>
2020-09-08 02:10:02 +0900

    add 404 pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from '../layout/default'
+
+const Index: React.FC  = props => {
+  return (
+    <Layout>
+        <div>404 not found</div>
+    </Layout>
+  )
+}
+
+export default Index
