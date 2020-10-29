1e1d32fb8222ad50f3c28ffc8557a6989d0063a3
Masaki Yoshiiwa <yopinoji@gmail.com>
2020-10-29 11:24:42 +0900

    fix Header style

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -5,9 +5,11 @@ import { H1 } from "@Components/atoms/Typography";
 const Header: React.FC = () => {
   return (
     <header>
-      <Link to="/">
-        <H1>{"Blog"}</H1>
-      </Link>
+      <div className="mx-12">
+        <Link to="/">
+          <H1>{"yopinoji.com"}</H1>
+        </Link>
+      </div>
     </header>
   );
 };
