9fcec0dc1a1a090972847f634929337d06982b64
Masaki Yoshiiwa <yopinoji@gmail.com>
2020-12-23 16:24:23 +0900

    add i18n support

---
+++ contents/2020-12-28-add-i18n-support-to-gatsby-for-markdown/index_ja.md
@@ -0,0 +1,15 @@
+---
+title: "Gatsby で作ったブログを i18n（多言語化）対応して Markdown を出し分ける"
+cover: "2020-12-28-add-i18n-support-to-gatsby-for-markdown/header.png"
+category: "Tech"
+lang: "ja"
+date: "2020-12-28"
+slug: "add-i18n-support-to-gatsby-for-markdown"
+tags:
+  - Gatsby
+  - i18n
+---
+
+詳細は以下に書いてあります。
+
+https://www.ietf.org/rfc/bcp/bcp47.txt
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,10 +11,40 @@ type PropsType = {
 
 const Index: React.VFC<PropsType> = (props) => {
   const siteMetadata = props.data.site?.siteMetadata;
+  const language =
+    (window.navigator.languages && window.navigator.languages[0]) ||
+    window.navigator.language;
+  const Posts = props.data.allMarkdownRemark.edges.filter((edge) => {
+    let filterdPost;
+    switch (language) {
+      case "en":
+      case "en-US":
+        filterdPost = edge.node.frontmatter?.lang === "en";
+        break;
+      case "ja":
+      case "ja-JP":
+        filterdPost = edge.node.frontmatter?.lang === "ja";
+        break;
+      case "zh-CN":
+      case "zh-Hans":
+      case "zh-SG":
+        filterdPost = edge.node.frontmatter?.lang === "en";
+        break;
+      case "zh-Hant":
+      case "zh-MO":
+      case "zh-HK":
+      case "zh-TW":
+        filterdPost = edge.node.frontmatter?.lang === "en";
+        break;
+      default:
+        return edge.node.frontmatter?.lang === "en";
+    }
+    return filterdPost;
+  });
   return (
     <Base siteMetadata={siteMetadata}>
       <SEO siteMetadata={siteMetadata} isPost={false} />
-      <PostListing data={props.data.allMarkdownRemark.edges} />
+      <PostListing data={Posts} />
     </Base>
   );
 };
@@ -41,6 +71,7 @@ export const pageQuery = graphql`
         node {
           frontmatter {
             category
+            lang
             cover
             date
             slug
