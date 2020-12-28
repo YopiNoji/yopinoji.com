f382eab8e7981997a832a8991957df9c673ad0f2
Masaki Yoshiiwa <yopinoji@gmail.com>
2020-12-28 15:04:47 +0900

    fix react spring animation

--- src/components/atoms/ScrollBar/ScrollBarY.tsx
+++ src/components/atoms/ScrollBar/ScrollBarY.tsx
@@ -14,11 +14,11 @@ const ScrollBarY: React.VFC = () => {
     document.addEventListener("scroll", onScroll);
     return (): void => document.removeEventListener("scroll", onScroll);
   });
+  const [springProps, setSpring] = useSpring(() => ({ height: `1%` }));
   const onScroll = () => {
     const position = scrollRatio();
     setSpring({ height: `${position}%` });
   };
-  const [springProps, setSpring] = useSpring(() => ({ height: `1%` }));
   return (
     <animated.div
       className="fixed w-6 top-0 right-0 bg-green-300 dark:bg-red-700"
