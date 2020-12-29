1d12ca5767261504e79fbde0709b733cfd4f7cc1
Masaki Yoshiiwa <yopinoji@gmail.com>
2020-12-29 14:43:43 +0900

    enable tailwind css in storybook

--- src/common.d.ts
+++ src/common.d.ts
@@ -1,7 +1,7 @@
 declare module "*.svg" {
-  import * as React from "react";
+  import { FunctionComponent } from "react";
 
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const ReactComponent: FunctionComponent<React.SVGProps<SVGSVGElement>>;
 
   export default ReactComponent;
 }
