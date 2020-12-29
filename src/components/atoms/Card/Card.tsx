1a53d6bbec01336fec18c7d037cc4243d6cb1bb5
Masaki Yoshiiwa <yopinoji@gmail.com>
2020-12-29 11:55:25 +0900

    change theme color

--- src/components/atoms/Card/Card.tsx
+++ src/components/atoms/Card/Card.tsx
@@ -4,7 +4,7 @@ export const Card: React.FC<HTMLAttributes<HTMLDivElement>> = ({
   ...props
 }) => {
   const className =
-    " border rounded overflow-hidden shadow-md m-2 p-2 transition duration-500 ease-in-out hover:shadow-2xl border-green-500 dark:border-red-500";
+    " border rounded overflow-hidden shadow-md m-2 p-2 transition duration-500 ease-in-out hover:shadow-2xl border-green-300 dark:border-pink-700";
   return (
     <div {...props} className={props.className + className}>
       {props.children}
